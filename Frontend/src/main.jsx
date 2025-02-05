fa7e7617f29688ba1f320dee5bdbe45ae03403e8
KhushiT-aptus <khushi.thacker@aptusdatalabs.com>
2025-02-05 22:41:31 +0530

    Update main.jsx

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,10 +1,8 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import ContextProvider from "./context/context.jsx";
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <ContextProvider>
-    <App />
-  </ContextProvider>
-);
+   <App />
+  );
