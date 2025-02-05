427c896b0131a2cf4fa540ff5090d9a008c8fd9c
KhushiT-aptus <khushi.thacker@aptusdatalabs.com>
2025-02-05 22:34:06 +0530

    Update SideBar.jsx

--- Frontend/src/Components/SideBar/SideBar.jsx
+++ Frontend/src/Components/SideBar/SideBar.jsx
@@ -1,38 +1,18 @@
 /* eslint-disable react/jsx-key */
 import { useContext } from "react";
 import "./SideBar.css";
-import { assets } from "../../assets/assets";
-import { Context } from "../../context/context";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faGear, faPlus } from "@fortawesome/free-solid-svg-icons";
+import { faGear } from "@fortawesome/free-solid-svg-icons";
 
 function Sidebar() {
-  // const { onSent, prevPrompts, setRecentPrompt, newChat } = useContext(Context);
-  // const loadPrompt = async (prompt) => {
-  //   setRecentPrompt(prompt);
-  //   await onSent(prompt);
-  // };
-
   return (
     <div className="sidebar">
       <div className="top">
-        <div>
-          <button className="new-chat">
-            <FontAwesomeIcon icon={faPlus} color="#8cc63e" size="lg" />
-            <p>New Chat</p>
-          </button>
-        </div>
-        {/* <div className="recent">
-          <p className="recent-title">Recent</p>
-          {prevPrompts.map((item, index) => {
-            return (
-              <div onClick={() => loadPrompt(item)} className="recent-entry">
-                <img src={assets.message_icon} alt="" />
-                <p>{item.slice(0, 18)} ...</p>
-              </div>
-            );
-          })}
-        </div> */}
+        <p className="sidebar-intro">
+          Welcome to your AI assistant! Explore insights, ask questions, and
+          interact with documents seamlessly. Let’s make your experience
+          efficient and engaging.
+        </p>
       </div>
       <div className="bottom">
         <div className="bottom-item">
